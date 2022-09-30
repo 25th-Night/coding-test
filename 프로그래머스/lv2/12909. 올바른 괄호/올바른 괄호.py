74547437762193f08eba3c6428d6370cf4b57e53
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-01 01:03:31 +0900

    [level 2] Title: 올바른 괄호, Time: 7.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,25 @@
-from collections import deque
+# from collections import deque
+
+# def solution(s):
+#     s = deque(s)
+#     arr = deque([])
+#     while s:
+#         if s[0] == "(":
+#             s.popleft()
+#             arr.append("(")
+#         else:
+#             if not arr:
+#                 return False
+#             elif arr[-1] == "(":
+#                 arr.pop()
+#                 s.popleft()
+#     return True if not arr else False
+
 
 def solution(s):
-    s = deque(s)
-    arr = deque([])
-    while s:
-        if s[0] == "(":
-            s.popleft()
-            arr.append("(")
-        else:
-            if not arr:
-                return False
-            elif arr[-1] == "(":
-                arr.pop()
-                s.popleft()
-    return True if not arr else False
+    x = 0
+    for w in s:
+        if x < 0:
+            break
+        x = x+1 if w=="(" else x-1 if w==")" else x
+    return x==0
