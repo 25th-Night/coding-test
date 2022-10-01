e56a534e8e438b7e6c0282fcd3c46315096e8104
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-01 22:53:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.35 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,30 @@
+# def solution(participant, completion):
+
+#     participant.sort()
+#     completion.sort()
+
+#     for i in range(len(completion)):
+#         if participant[i] != completion[i]:
+#             return participant[i]
+
+
+# def solution(participant, completion):
+#     dic = {i:0 for i in participant}
+#     for i in participant:
+#         dic[i] += 1
+#     for j in completion:
+#         dic[j] -= 1
+#     for k in completion:
+#         if dic[k] == 0:
+#             return k
+       
+    
+def solution(participant, completion):
+    dic = {}
+    for i in participant:
+        dic[i] = dic.get(i, 0) + 1
+    for j in completion:
+        dic[j] -= 1
+    for k in participant:
+        if dic[k] != 0:
+            return k
