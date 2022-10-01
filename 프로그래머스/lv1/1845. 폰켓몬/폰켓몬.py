22196647df0c61a64d7fb7c07eec23a84c3c77f8
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-01 22:28:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,2 @@
-def solution(nums):
-    l1 = len(nums)
-    num2 = set(nums)
-    l2 = len(num2)
-    return l2 if l1//2 >= l2 else l1//2
+def solution(ls):
+        return min(len(ls)//2, len(set(ls)))
