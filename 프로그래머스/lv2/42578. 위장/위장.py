46329c488fb1a33b262074c98a0db90ac305596c
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-05 10:22:18 +0900

    [level 2] Title: 위장, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -1,9 +1,18 @@
-import math
+# import math
+
+# def solution(clothes):
+#     answer = {}
+#     for p in clothes:
+#         answer[p[1]] = answer.get(p[1], 0) + 1
+#     array = answer.values()
+#     array = [i+1 for i in array]
+#     return math.prod(array) -1
+
+
+from collections import Counter
+from functools import reduce
 
 def solution(clothes):
-    answer = {}
-    for p in clothes:
-        answer[p[1]] = answer.get(p[1], 0) + 1
-    array = answer.values()
-    array = [i+1 for i in array]
-    return math.prod(array) -1
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    return answer
