57cb0cf9b131b0b99f60dda5048fba45408b92f9
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-05 10:08:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.54 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,37 +1,37 @@
-# def solution(participant, completion):
-
-#     participant.sort()
-#     completion.sort()
-
-#     for i in range(len(completion)):
-#         if participant[i] != completion[i]:
-#             return participant[i]
-
 def solution(participant, completion):
+
     participant.sort()
     completion.sort()
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
 
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
 
 # def solution(participant, completion):
-#     dic = {i:0 for i in participant}
-#     for i in participant:
-#         dic[i] += 1
-#     for j in completion:
-#         dic[j] -= 1
-#     for k in participant:
-#         if dic[k] != 0:
-#             return k
+#     participant.sort()
+#     completion.sort()
+#     for p, c in zip(participant, completion):
+#         if p != c:
+#             return p
+
+
+# # def solution(participant, completion):
+# #     dic = {i:0 for i in participant}
+# #     for i in participant:
+# #         dic[i] += 1
+# #     for j in completion:
+# #         dic[j] -= 1
+# #     for k in participant:
+# #         if dic[k] != 0:
+# #             return k
        
     
-# def solution(participant, completion):
-#     dic = {}
-#     for i in participant:
-#         dic[i] = dic.get(i, 0) + 1
-#     for j in completion:
-#         dic[j] -= 1
-#     for k in participant:
-#         if dic[k] != 0:
-#             return k
+# # def solution(participant, completion):
+# #     dic = {}
+# #     for i in participant:
+# #         dic[i] = dic.get(i, 0) + 1
+# #     for j in completion:
+# #         dic[j] -= 1
+# #     for k in participant:
+# #         if dic[k] != 0:
+# #             return k
