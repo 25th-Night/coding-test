0f5ac42dba5b142712d0857faacf2610700f4efa
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-10 18:33:07 +0900

    [level 2] Title: 올바른 괄호, Time: 7.47 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,24 +1,33 @@
 from collections import deque
 
+# def solution(s):
+#     # 주어진 문자열
+#     s = deque(s)
+#     # stack으로 활용할 문자열
+#     arr = deque([])
+#     while s:
+#         if s[0] == "(":
+#             s.popleft()
+#             arr.append("(")
+#         else:
+#             if not arr:
+#                 return False
+#             elif arr[-1] == "(":
+#                 arr.pop()
+#                 s.popleft()
+#     return not arr
+
 def solution(s):
-    # 주어진 문자열
-    s = deque(s)
-    # stack으로 활용할 문자열
-    arr = deque([])
-    while s:
-        if s[0] == "(":
-            s.popleft()
-            arr.append("(")
+    answer = []
+    for i in s:
+        if i == "(":
+            answer.append(i)
         else:
-            if not arr:
+            if not answer:
                 return False
-            elif arr[-1] == "(":
-                arr.pop()
-                s.popleft()
-    return not arr
-
-
-
+            elif answer[-1] == "(":
+                answer.pop()
+    return not answer
 
 
 
