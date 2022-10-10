ff267e5d9714548e232f46d8e41b26d25a6fd9ea
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-10 20:33:54 +0900

    [level 2] Title: 프린터, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,13 +1,33 @@
+# from collections import deque
+
+# def solution(priorities, location):
+#     priorities = deque([[idx, priority] for idx, priority in enumerate(priorities)])
+#     stack = []
+#     while priorities:
+#         target = priorities.popleft()
+#         if any(target[1] < priority[1] for priority in priorities):
+#             priorities.append(target)
+#         else:
+#             stack.append(target)
+#     for i in range(len(stack)):
+#         if location == stack[i][0]:
+#             return i+1
+      
+from collections import deque
+        
 def solution(priorities, location):
-    target = priorities[location]
-    print(target)
-    while priorities:
-        J = priorities[0]
-        if J < max(priorities[1:]):
-            J = priorities.pop(0)
-            priorities.append(J)
+    priorities = deque([(idx, priority) for idx, priority in enumerate(priorities)])
+    answer = 0
+    while True:
+        current_priority = priorities.popleft()
+        if any(current_priority[1] < priority[1] for priority in priorities):
+            priorities.append(current_priority)
+            # print(current_priority)
+            # print(priorities)
         else:
-            break
-    print(priorities)
-    
-    return priorities.index(target)+1
+            answer += 1
+            # print(current_priority)
+            # print(priorities)
+            # print(answer)
+            if current_priority[0] == location :
+                return answer
