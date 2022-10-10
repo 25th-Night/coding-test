99b40d930596d4eb8d27a7c6c83982835fe68cee
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-10 23:57:45 +0900

    [level 2] Title: 주식가격, Time: 18.49 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,12 +1,34 @@
-from collections import deque
+# from collections import deque
+
+# def solution(prices):
+#     length = len(prices)
+#     answer = [0] * length
+    
+#     for i in range(length):
+#         for j in range(i+1, length):
+#             answer[i] +=1
+#             if prices[i] > prices[j]:
+#                 break
+#     return answer
+
 
 def solution(prices):
-    length = len(prices)
-    answer = [0] * length
-    
-    for i in range(length):
-        for j in range(i+1, length):
-            answer[i] +=1
-            if prices[i] > prices[j]:
-                break
+    stack = []
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        # print(f"i : {i}")
+        while stack and stack[-1][1] > prices[i]:
+            past, _ = stack.pop()
+            # print(f"past : {past}")
+            # print(f"_ : {_}")
+            answer[past] = i - past
+            # print(f"answer : {answer}")
+        stack.append([i, prices[i]])
+        # print(f"stack : {stack}")
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
+        # print(f"answer : {answer}")
     return answer
+
+prices = [1,2,3,2,3]
+solution(prices)
