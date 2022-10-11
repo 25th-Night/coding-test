546d6d7129827632f77bf10187a3fd44b461a451
25th-Night <104040502+25th-Night@users.noreply.github.com>
2022-10-11 09:48:07 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,20 +1,20 @@
 def solution(citations):
-    citations.sort()
-    idx = len(citations) // 2
-    target = citations[idx]
-    while True:
-        right = [num for num in citations if num >= target]
-        if len(right) >= target:
-            return target
-        else:
-            target -= 1
-    return target
+    citations.sort(reverse=True)
+    for idx, citation in enumerate(citations):
+        if idx >= citation:
+            return idx
+    return len(citations)
 
 
 # def solution(citations):
 #     citations = sorted(citations)
+#     print(citations)
 #     l = len(citations)
+#     print(l)
 #     for i in range(l):
 #         if citations[i] >= l-i:
+#             print(f"i:{i}")
+#             print(citations[i])
+#             print(l-i)
 #             return l-i
 #     return 0
