e1501f76f3d36bad0c4de91ef5f453533648c74c
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-01 21:52:33 +0900

    [level 4] Title: 도둑질, Time: 562.13 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -0,0 +1,15 @@
+def solution(money):
+    dp1 = [0] * len(money)
+    dp1[0], dp1[1] = money[0], max(money[0], money[1])
+    
+    # 첫 집을 무조건 터는 경우
+    for i in range(2, len(money) - 1):
+        dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i])
+    
+    # 마지막 집을 무조건 터는 경우
+    dp2 = [0] * len(money)
+    dp2[0], dp2[1] = 0, money[1]
+    for i in range(2, len(money)):
+        dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i])
+
+    return max(max(dp1), max(dp2))
