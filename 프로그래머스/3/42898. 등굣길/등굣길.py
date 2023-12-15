8e5d12060ac463e01c4d1b208cb7a6273c565ed0
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-15 21:51:48 +0900

    [level 3] Title: 등굣길, Time: 1.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,28 +1,17 @@
 def solution(m, n, puddles):
-    map = [[1 for j in range(m)] for i in range(n)]
-
-    # 상단, 좌측 라인의 경우, 우물부터 끝까지 좌표값을 0으로 변경
-    for x, y in puddles:
-        if x == 1:
-            for i in range(y-1, n):
-                map[i][x-1] = 0
-        if y == 1:
-            for i in range(x-1, m):
-                map[y-1][i] = 0
-
-    # 우물 좌표값을 0으로 변경
-    for x, y in puddles:
-        map[y-1][x-1] = 0
-
-    for x in range(m):
-        for y in range(n):
-            # 범위 벗어나는 경우
-            if x-1 < 0 or y-1 < 0:
-                continue
-            # 우물은 건너뛰기
-            if map[y][x] == 0:
-                continue
-            # 해당 좌표값은 좌측 좌표와 상단 좌표의 좌표값의 합
-            map[y][x] = map[y][x-1] + map[y-1][x]
-
-    return map[n-1][m-1] % 1000000007
+    puddles = [[r-1, c-1] for r, c in puddles]
+    maps = [[1] * m for _ in range(n)]
+    for c, r in puddles:
+        if not r:
+            for i in range(c, m):
+                maps[r][i] = 0
+        elif not c:
+            for i in range(r, n):
+                maps[i][c] = 0
+        else:
+            maps[r][c] = 0
+    for i in range(1, n):
+        for j in range(1, m):
+            maps[i][j] = maps[i][j-1] + maps[i-1][j] if maps[i][j] else 0
+                
+    return maps[-1][-1] % 1000000007
