9b7ed13281698c0de0c71ff146be0a91068f5bcf
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-15 22:12:24 +0900

    [level 4] Title: 도둑질, Time: 554.56 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897) 
 
 ### 성능 요약
 
-메모리: 43.4 MB, 시간: 298.21 ms
+메모리: 88.3 MB, 시간: 554.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 21:57:14
+2023년 12월 5일 22:12:23
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,11 +1,12 @@
 def solution(money):
-    # 첫 집을 무조건 터는 경우
-    x1, y1, z1 = money[0], money[1], money[0] + money[2]
-    # 마지막 집을 무조건 터는 경우
-    x2, y2, z2 = 0, money[1], money[2]
+    dp1 = [0] * len(money)
+    dp1[0], dp1[1] = money[0], max(money[0], money[1])
+    for i in range(2, len(money)-1):
+        dp1[i] = max(dp1[i-1], dp1[i-2] + money[i])
     
-    for m in money[3:]:
-        # 다음 집들 털면서 업데이트
-        x1, y1, z1 = y1, z1, max(x1, y1) + m
-        x2, y2, z2 = y2, z2, max(x2, y2) + m
-    return max(x1, y1, y2, z2)
+    dp2 = [0] * len(money)
+    dp2[0], dp2[1] = 0, money[1]
+    for i in range(2, len(money)):
+        dp2[i] = max(dp2[i-1], dp2[i-2] + money[i])
+        
+    return max(max(dp1), max(dp2))
