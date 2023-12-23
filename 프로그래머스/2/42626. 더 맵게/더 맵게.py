aed57cdaf265afeb73257071715021870f433d01
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-23 14:03:33 +0900

    [level 2] Title: 더 맵게, Time: 1824.21 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,30 +1,13 @@
-
-from collections import deque
+from heapq import heapify, heappop, heappush
 
 def solution(scoville, K):
-    scoville.sort()
-    scoville = deque(scoville)
-    
-    result = 0
-    mixed = deque()
-    
-    while (scoville and scoville[0] < K) or (mixed and mixed[0] < K):
-        result += 1
-        if len(scoville) + len(mixed) <= 1:
+    heapify(scoville)
+    cnt = 0
+    while scoville[0] < K:
+        if len(scoville) <= 1:
             return -1
-        
-        food = [0] * 2
-        for i in range(2):
-            if scoville and mixed:
-                if scoville[0] < mixed[0]:
-                    food[i] = scoville.popleft()
-                else:
-                    food[i] = mixed.popleft()
-            elif scoville:
-                food[i] = scoville.popleft()
-            else:
-                food[i] = mixed.popleft()
-            
-        mixed.append(food[0] + food[1] * 2)
-        
-    return result
+        f = heappop(scoville)
+        s = heappop(scoville)
+        heappush(scoville, f+s*2)
+        cnt += 1
+    return cnt
