c6d7ef8a56ee1db503d3c796baad54b0e47c7c4b
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-23 09:22:01 +0900

    [level 4] Title: 사칙연산, Time: 113.11 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,21 +1,19 @@
 def solution(arr):
-    num_cnt = len(arr) // 2 + 1
-    nums = [int(num) for num in arr[::2]]
-    ops = [op for op in arr[1::2]]
-    dp_M = [[-float("inf")] * num_cnt for _ in range(num_cnt)]
-    dp_m = [[float("inf")] * num_cnt for _ in range(num_cnt)]
-    for i in range(num_cnt):
-        dp_M[i][i] = dp_m[i][i] = nums[i]
-
-    for length in range(1, num_cnt):
-        for i in range(num_cnt - length):
+    cnt = len(arr) // 2 + 1
+    nums = [int(n) for n in arr[::2]]
+    ops = [o for o in arr[1::2]]
+    dpM = [[-float("inf")] * cnt for _ in range(cnt)]
+    dpm = [[float("inf")] * cnt for _ in range(cnt)]
+    for i in range(cnt):
+        dpM[i][i] = dpm[i][i] = nums[i]
+    for length in range(1, cnt):
+        for i in range(cnt - length):
             j = i + length
             for k in range(i, j):
                 if ops[k] == "+":
-                    dp_M[i][j] = max(dp_M[i][j], dp_M[i][k] + dp_M[k+1][j])
-                    dp_m[i][j] = min(dp_m[i][j], dp_m[i][k] + dp_m[k+1][j])
+                    dpM[i][j] = max(dpM[i][j], dpM[i][k] + dpM[k+1][j])
+                    dpm[i][j] = min(dpm[i][j], dpm[i][k] + dpm[k+1][j])
                 elif ops[k] == "-":
-                    dp_M[i][j] = max(dp_M[i][j], dp_M[i][k] - dp_m[k+1][j])
-                    dp_m[i][j] = min(dp_m[i][j], dp_m[i][k] - dp_M[k+1][j])
-            
-    return dp_M[0][-1]
+                    dpM[i][j] = max(dpM[i][j], dpM[i][k] - dpm[k+1][j])
+                    dpm[i][j] = min(dpm[i][j], dpm[i][k] - dpM[k+1][j])
+    return dpM[0][-1]
