dc95c2da150c9294b9d7b9e6efc8a2e2f27cccff
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-23 14:15:34 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,21 +1,19 @@
 from heapq import heappop, heappush
 
-def solution(jobs): 
-    result = 0
-    start, now = -1, 0
-    idx = 0
+def solution(jobs):
+    s, e = -1, 0
+    f, t = 0, 0
     heap = []
-
-    while idx < len(jobs):
-        for job_start, job_time in jobs:
-            if start < job_start <= now:
-                heappush(heap, [job_time, job_start])
+    while f < len(jobs):
+        for job in jobs:
+            if s < job[0] <= e:
+                heappush(heap, (job[1], job[0]))
         if heap:
-            before_job_time, before_job_start = heappop(heap)
-            start = now
-            now += before_job_time
-            result += now - before_job_start
-            idx += 1
+            cn, cs = heappop(heap)
+            s = e
+            e += cn
+            t += e - cs
+            f += 1
         else:
-            now += 1
-    return result // idx
+            e += 1
+    return t // f
