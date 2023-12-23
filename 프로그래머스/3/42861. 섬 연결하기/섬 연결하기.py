361a7b48dbe88e66f863420babbe31d566bbf33c
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-23 09:35:23 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -3,28 +3,20 @@ def get_parent(parent, x):
         parent[x] = get_parent(parent, parent[x])
     return parent[x]
 
-def union_parent(parent, a, b):
-    a = get_parent(parent, a)
-    b = get_parent(parent, b)
-    if a<b:
+def union_parent(parent, x, y):
+    a = get_parent(parent, x)
+    b = get_parent(parent, y)
+    if a < b:
         parent[b] = a
     else:
         parent[a] = b
-        
-def find_parent(parent, a, b):
-    a = get_parent(parent, a)
-    b = get_parent(parent, b)
-    
-    return a == b
 
 def solution(n, costs):
-    answer = 0
+    costs.sort(key=lambda x:x[2])
     parent = list(range(n))
-    costs.sort(key = lambda x : x[2])
-
-    for node1, node2, cost in costs:
-        if get_parent(parent, node1) != get_parent(parent, node2):
-            union_parent(parent, node1, node2)
+    answer = 0
+    for n1, n2, cost in costs:
+        if get_parent(parent, n1) != get_parent(parent, n2):
+            union_parent(parent, n1, n2)
             answer += cost
-
     return answer
