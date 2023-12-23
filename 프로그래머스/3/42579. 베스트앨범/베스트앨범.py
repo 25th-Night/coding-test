b9fac00073e0f06eed3e32509acdc4b428650ab4
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-23 16:23:29 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,23 +1,20 @@
 from collections import defaultdict
 
-def sum_of_plays(genre, genres_info):
-    filtered_genres_info = filter(lambda x: genre == x[0], genres_info)
-    total_cnt = sum(map(lambda x: x[1], filtered_genres_info))
-    return total_cnt
-
 def solution(genres, plays):
-    genres_info = [(genre, play) for genre, play in zip(genres, plays)]
-    genres_info = [(genre, play, sum_of_plays(genre, genres_info), i) for i, (genre, play) in enumerate(genres_info)]
-    genres_info.sort(key=lambda x: (-x[2], -x[1], x[3]))
-    genres_dict = defaultdict(int)
+    total = defaultdict(int)
+    for g, p in zip(genres, plays):
+        total[g] += p
+
+    info = []
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        info.append([g, total[g], p, i])
+
+    info.sort(key=lambda x:(-x[1], -x[2], x[3]))
+
+    check = defaultdict(int)
     result = []
-    for genre_info in genres_info:
-        if genres_dict[genre_info[0]] == 0:
-            genres_dict[genre_info[0]] = 1
-            result.append(genre_info[3])
-        elif genres_dict[genre_info[0]] == 1: 
-            genres_dict[genre_info[0]] = 2
-            result.append(genre_info[3])
-        elif genres_dict[genre_info[0]] == 2:
-            continue
+    for g, t, p, i in info:
+        if check[g] < 2:
+            result.append(i)
+            check[g] += 1
     return result
