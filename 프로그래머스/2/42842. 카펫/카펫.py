5b3b5bd1bdf81607dcf83568a50aee1299e966e3
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-23 13:32:44 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,5 @@
 def solution(brown, yellow):
-    total_cnt = brown + yellow
-    for i in range(total_cnt // 2, int(total_cnt**0.5) - 1, -1):
-        if not total_cnt % i:
-            width, height = i, total_cnt // i
-            if (width - 2)*(height - 2) == yellow:
-                return [width, height]
+    total = brown + yellow
+    for n in range(total//3, int(total**0.5)-1, -1):
+        if not total % n and 2 * (n + total//n) - 4 == brown:
+            return [n, total//n]
