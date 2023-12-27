779dfd90c69a46ab0d2fe667aebad46e1b131af5
Sun <104040502+25th-Night@users.noreply.github.com>
2023-12-27 16:25:51 +0900

    [level 3] Title: 가장 먼 노드, Time: 67.66 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,14 +1,12 @@
 from collections import defaultdict
 
-def make(edge):
-    g = defaultdict(list)
-    for a, b in edge:
-        g[a].append(b)
-        g[b].append(a)
-    return g
+def make_graph(edge):
+    graph = defaultdict(list)
+    [graph[a].append(b) == graph[b].append(a) for a, b in edge]
+    return graph
 
 def bfs(graph, visited, dist, start):
-    q= [start]
+    q = [start]
     while q:
         v = q.pop(0)
         visited[v] = True
@@ -19,8 +17,8 @@ def bfs(graph, visited, dist, start):
                 dist[n] = dist[v] + 1
 
 def solution(n, edge):
+    graph = make_graph(edge)
     visited = [False] * (n+1)
-    graph = make(edge)
     dist = [0] * (n+1)
     bfs(graph, visited, dist, 1)
     return dist.count(max(dist))
