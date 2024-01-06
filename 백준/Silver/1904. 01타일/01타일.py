d9f39ee914a11c2e9648b887dd599cf81172f48f
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-06 09:16:08 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,10 +1,11 @@
 N = int(input())
 
-dp = [0] * 1000001
-dp[1] = 1
-dp[2] = 2
-
-for i in range(3, N+1):
-    dp[i] = (dp[i-1] + dp[i-2]) % 15746
-
-print(dp[N])
+if N <= 2:
+    print(N)
+else:
+    a, b = 1, 2
+    n = 3
+    while n <= N:
+        a, b = b % 15746, (a + b) % 15746
+        n += 1
+    print(b)
