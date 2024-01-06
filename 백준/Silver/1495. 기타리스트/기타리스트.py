fbd9ec96153c95f776e8fbeb7e4e969e0cccf39d
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-06 10:13:31 +0900

    [Silver I] Title: 기타리스트, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -0,0 +1,22 @@
+n, s, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+dp = [[False] * (m+1) for _ in range(n+1)]
+dp[0][s] = True
+
+for i in range(1, n+1):
+    for j in range(m+1):
+        if dp[i-1][j] == False:
+            continue
+        if j - arr[i-1] >= 0:
+            dp[i][j-arr[i-1]] = True
+        if j + arr[i-1] <= m:
+            dp[i][j+arr[i-1]] = True
+
+result = -1
+for i in range(m, -1, -1):
+    if dp[n][i]:
+        result = i
+        break
+
+print(result)
