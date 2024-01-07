a70ed923eab2bb802f24c34b54f6989622baafa1
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-07 18:46:54 +0900

    [Gold V] Title: 배, Time: 128 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,34 +1,24 @@
 import sys
+input = sys.stdin.readline
 
-n = int(input())
+N = int(input())
 cranes = sorted(map(int, input().split()), reverse=True)
-
-m = int(input())
+M = int(input())
 boxes = sorted(map(int, input().split()), reverse=True)
 
+cnt = 0
+
 if cranes[0] < boxes[0]:
-    print(-1)
-    sys.exit()
+    cnt = -1
+else:
+    while boxes:
+        for c in cranes:
+            if boxes and c < boxes[-1]:
+                continue
+            for b in boxes:
+                if c >= b:
+                    boxes.remove(b)
+                    break
+        cnt += 1
 
-
-positions = [0] * n
-
-checked = [False] * m
-
-result = 0
-count = 0
-
-while True:
-    if count == len(boxes):
-        break
-    for i in range(n):
-        while positions[i] < len(boxes):
-            if not checked[positions[i]] and cranes[i] >= boxes[positions[i]]:
-                checked[positions[i]] = True
-                positions[i] += 1
-                count += 1
-                break
-            positions[i] += 1
-    result += 1
-
-print(result)
+print(cnt)
