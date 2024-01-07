be323f9c3440ab5e83bcd77376c75386c2f68b62
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-07 15:57:12 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 114244 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -3,33 +3,32 @@ from collections import deque
 
 input = sys.stdin.readline
 
-def bfs(r, c):
-    q = deque([(r, c)])
-    move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    while q:
-        cr, cc = q.popleft()
+def bfs(graph):
+    cnt = 0
+    for r in range(len(graph)):
+        for c in range(len(graph[0])):
+            if graph[r][c] == 1:
+                cnt += 1
+                q = deque([(r, c)])
+                move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+                while q:
+                    cr, cc = q.popleft()
 
-        for r, c in move:
-            nr, nc = cr+r, cc+c
+                    for dr, dc in move:
+                        nr, nc = cr + dr, cc + dc
 
-            if nr in range(len(graph)) and nc in range(len(graph[0])) and graph[nr][nc]:
-                q.append((nr, nc))
-                graph[nr][nc] = 0
-    return 1
+                        if nr in range(len(graph)) and nc in range(len(graph[0])) and graph[nr][nc]:
+                            q.append((nr, nc))
+                            graph[nr][nc] = 0
+    return cnt
 
 
 for _ in range(int(input())):
     m, n, k = map(int, input().split())
-    graph = [[0 for _ in range(m)] for _ in range(n)]
+    graph = [[0] * m for _ in range(n)]
 
     for i in range(k):
         c, r = map(int, input().split())
         graph[r][c] = 1
 
-    cnt = 0
-    for r in range(n):
-        for c in range(m):
-            if graph[r][c] == 1:
-                cnt += bfs(r, c)
-
-    print(cnt)
+    print(bfs(graph))
