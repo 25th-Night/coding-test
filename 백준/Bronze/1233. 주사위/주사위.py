046f3946ee2d5f08a9dfcced6deb02311ba5b339
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-13 15:11:06 +0900

    [Bronze II] Title: 주사위, Time: 108 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/1233. 주사위/주사위.py
@@ -0,0 +1,10 @@
+a, b, c = map(int, input().split())
+
+dp = [0] * 81
+
+for i in range(1, a+1):
+    for j in range(1, b+1):
+        for k in range(1, c+1):
+            dp[i+j+k] += 1
+
+print(dp.index(max(dp)))
