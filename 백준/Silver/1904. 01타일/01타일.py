d80efcb3b22a2b54c9ded952f08cdfb0c79560e4
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-18 19:16:25 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,11 +1,10 @@
 N = int(input())
 
-if N <= 2:
-    print(N)
-else:
-    a, b = 1, 2
-    n = 3
-    while n <= N:
-        a, b = b % 15746, (a + b) % 15746
-        n += 1
-    print(b)
+a, b = 1, 2
+n = 1
+
+while n < N:
+    a, b = b, (a+b) % 15746
+    n += 1
+
+print(a)
