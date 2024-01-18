11caf14ff33edf1efc01019f5520697d1cb645e5
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-18 19:31:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 312 ms, Memory: 189588 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,4 +1,5 @@
 N, K = map(int, input().split())
+
 dp = [[0] * (K+1) for _ in range(N+1)]
 
 for i in range(1, N+1):
@@ -8,5 +9,5 @@ for i in range(1, N+1):
             dp[i][j] = dp[i-1][j]
         else:
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
-
+        
 print(dp[N][K])
