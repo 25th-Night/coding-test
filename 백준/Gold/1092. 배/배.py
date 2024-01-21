ba77ca84211e0e6a8229d6fb106e1f3525769e80
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-21 12:36:09 +0900

    [Gold V] Title: 배, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,24 +1,23 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-cranes = sorted(map(int, input().split()), reverse=True)
+cranes = list(map(int, input().split()))
 M = int(input())
-boxes = sorted(map(int, input().split()), reverse=True)
+boxes = list(map(int, input().split()))
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
 
-cnt = 0
-
+t = 0
 if cranes[0] < boxes[0]:
-    cnt = -1
+    t = -1
 else:
     while boxes:
         for c in cranes:
             if boxes and c < boxes[-1]:
-                continue
+                break
             for b in boxes:
                 if c >= b:
                     boxes.remove(b)
                     break
-        cnt += 1
+        t += 1
 
-print(cnt)
+print(t)
+
