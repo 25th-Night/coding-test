9297635d88f158c863ba3ca321b3da0fbf7f4d4f
Sun <104040502+25th-Night@users.noreply.github.com>
2024-01-21 13:23:37 +0900

    [Gold IV] Title: 도서관, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -1,27 +1,21 @@
-from heapq import heappop, heappush
-
 N, M = map(int, input().split())
-bs = list(map(int, input().split()))
-longest = max(max(bs), -min(bs))
-p, n = [], []
+books = sorted(map(int, input().split()))
+longest = max(max(books), -min(books))
 
-for b in bs:
-    if b > 0:
-        heappush(p, -b)
+plus, minus = [], []
+minus = []
+for book in books:
+    if book > 0:
+        plus.append(book)
     else:
-        heappush(n, b)
-        
-result = 0
+        minus.append(book)
+plus.sort(reverse=True)
+minus.sort()
 
-while p:
-    result += heappop(p)
-    for _ in range(M-1):
-        if p:
-            heappop(p)
-while n:
-    result += heappop(n)
-    for _ in range(M-1):
-        if n:
-            heappop(n)
-            
-print(-2*result - longest)
+total = 0
+for p in plus[::M]:
+    total += 2*p
+for m in minus[::M]:
+    total += -2*m
+
+print(total - longest)
